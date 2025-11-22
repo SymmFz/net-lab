79e97428adaf5f3b24592c543a2da2ef6a129183
symmfz <symmfz@outlook.com>
2025-11-23 02:02:52 +0800

    chore: udp_server setup

---
+++ app/udp_server_env_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo ip link del veth_host 2>/dev/null
+sudo ip netns del ns_test  2>/dev/null
+
+sudo ip link add veth_host type veth peer name veth_ns
+sudo ip addr add 192.168.100.3/24 dev veth_host
+sudo ip link set veth_host up
+sudo ethtool -K veth_host tx off
+
+sudo ip netns add ns_test
+sudo ip link set veth_ns netns ns_test
+sudo ip netns exec ns_test bash -c "
+    ip link set veth_ns up
+    ip addr add 192.168.100.1/24 dev veth_ns
+    ethtool -K veth_ns tx off
+"
+
+sudo ip netns exec ns_test socat -x - UDP4:192.168.100.2:60000
--- include/config.h
+++ include/config.h
@@ -13,7 +13,7 @@
 #else
 #define NET_IF_IP        \
     {                    \
-        10, 250, 162, 179 \
+        192, 168, 100, 2 \
     }  // 自定义网卡ip地址
 #define NET_IF_MAC                         \
     {                                      \
